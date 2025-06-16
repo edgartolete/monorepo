964f388ca9f3b33699212990f2f80bc24040abd6
Edgar Tolete <edgartolete@outlook.com>
2025-06-17 00:41:07 +0800

    chore: added web app for testing

--- packages/react/src/components/button/index.tsx
+++ packages/react/src/components/button/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-export function Button({ children, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  ref?: React.Ref<HTMLButtonElement>
+}
+
+export function Button({ children, ...props }: ButtonProps) {
   return (
     <button {...props} className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600'>
       {children}
