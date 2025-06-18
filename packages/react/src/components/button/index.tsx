f34e1cbf9fbe5df95619f2f2746d2500f85b7d6d
Edgar Tolete <edgartolete@outlook.com>
2025-06-19 02:04:52 +0800

    chore: cleanup

--- packages/react/src/components/button/index.tsx
+++ packages/react/src/components/button/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-import styles from './style.module.css'
+import style from './button.module.css'
 import clsx from 'clsx'
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   ref?: React.Ref<HTMLButtonElement>
+  pill?: boolean
 }
 
-export default function Button({ children, className, ...props }: ButtonProps) {
+export default function Button({ children, pill, className, ...props }: ButtonProps) {
   return (
-    <button {...props} className={clsx(styles.button, className)}>
+    <button {...props} className={clsx(style.base, pill && style.pill, className)}>
       {children}
     </button>
   )
 }
-export const val = 'button'
