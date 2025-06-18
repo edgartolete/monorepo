7b7c7a97e581e9523fef0388694cdb13469b9152
Edgar Tolete <edgartolete@outlook.com>
2025-06-19 01:43:19 +0800

    fix(CSS MODULE): manage to fix css module on package
    fix: updated app name

--- packages/react/.storybook/main.ts
+++
@@ -1,9 +0,0 @@
-import type { StorybookConfig } from '@storybook/nextjs'
-
-const config: StorybookConfig = {
-  // ...
-  // framework: '@storybook/react-webpack5', 👈 Remove this
-  framework: '@storybook/nextjs' // 👈 Add this
-}
-
-export default config
--- packages/react/src/components/button/index.tsx
+++ packages/react/src/components/button/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
+import styles from './style.module.css'
+import clsx from 'clsx'
+
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   ref?: React.Ref<HTMLButtonElement>
 }
 
-export function Button({ children, className, ...props }: ButtonProps) {
+export default function Button({ children, className, ...props }: ButtonProps) {
   return (
-    <button {...props} className={className}>
+    <button {...props} className={clsx(styles.button, className)}>
       {children}
     </button>
   )
 }
+export const val = 'button'
